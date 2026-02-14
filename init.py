88faabd863b9d34618f3d935c4fcb5bc587006ad
Jonathan Liu <jonliu168@gmail.com>
2026-02-14 11:53:53 -0800

    test endpoint works

--- init.py
+++ init.py
@@ -1,6 +1,11 @@
 from dotenv import load_dotenv
+from flask import Flask
+from flask_cors import CORS
 import os
 load_dotenv()
 SERVER_NAME = "Relay Server"
 DATABASE_NAME = os.getenv("DATABASE_NAME")
 JWT_KEY = os.getenv("JWT_KEY")
+
+app = Flask(SERVER_NAME)
+cors = CORS(app=app,supports_credentials=True)
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import sqlite3
 import init
+from init import app, cors
 import crypt
 
-app = Flask(init.SERVER_NAME)
 connection = sqlite3.connect(f"{init.DATABASE_NAME}.db")
+
+@app.before_request
+def before_request(): 
+    allowed_origin = request.headers.get('Origin')
+    if allowed_origin in ['http://localhost:4100']:
+        cors._origins = allowed_origin
 
 @app.route("/")
 def home():
     return "eventually lets put something cool here"
 
-app.run()
+@app.route("/test",methods=["POST"])
+def test():
+
+    data = request.get_json()
+    
+    return data
+
+app.run(port=8080)
