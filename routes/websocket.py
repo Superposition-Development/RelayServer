bde91cedfccd46069c3fd2534d1e03cc67f188f7
Jonathan Liu <jonliu168@gmail.com>
2026-03-03 10:46:31 -0800

    user validation in websocket that needs to be secured because this may be insecure and cause a vuln

--- routes/websocket.py
+++ routes/websocket.py
@@ -1,18 +1,27 @@
 from init import socketApp
-from flask import jsonify
 import json
+from AuthKeyGen import decryptJWT
+import database
 
 connections = {}
-print("breh")
 
 @socketApp.route("/ws")
 def websocket(ws):
     while True: 
-        print("breh")
         data = json.loads(ws.receive())
 
-        if not "message" in data:
+        if not "message" in data or "authKey" not in data:
             continue
+
+        result = decryptJWT(data["authKey"])
+
+        if not ("Result" in result.keys()):
+            continue
+
+        user = database.queryTableValue(["id","pfp","username","userID","password","timestamp"],"user","userID",result["Result"]["userID"])
+
+        if not(user["userID"] in connections.keys()): #this might cause a vuln eventually if you can find a way to change the value of the websocket
+            connections[user["userID"]] = ws
 
         response = {"message":""}
 
