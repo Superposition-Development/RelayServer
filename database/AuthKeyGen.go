e5accdd6c216ee87842b454000411ee260abc310
Jonathan Liu <jonliu168@gmail.com>
2026-04-02 12:25:08 -0700

    decrypt jwt stuff i stole and now need to rewrite

--- database/AuthKeyGen.go
+++ database/AuthKeyGen.go
@@ -1,11 +1,38 @@
 package database
 
 import (
-	JWT "github.com/golang-jwt/jwt/v5"
+	"errors"
+	"fmt"
+
+	jwt "github.com/golang-jwt/jwt/v5"
 )
 
-func decryptJWT(token string) {
-	JWT.Parse()
+type MyClaims struct {
+	jwt.RegisteredClaims
+	Username string `json:"username"`
+}
+
+func decryptJWT(tokenString string) (*jwt.Token, error) {
+	secretKey := []byte(config.SecretKey)
+
+	token, err := jwt.ParseWithClaims(tokenString, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return secretKey, nil
+	})
+
+	if err != nil {
+		fmt.Println("Error parsing token:", err)
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*MyClaims); ok && token.Valid {
+		fmt.Println("Token valid for user:", claims.Username)
+	} else {
+		fmt.Println("Invalid token")
+	}
+	return token, nil
 }
 
 func encryptJWT() {
