126b86a0a3973ecf364f607b21344ba1e55e34e5
Jonathan Liu <jonliu168@gmail.com>
2026-05-18 10:21:05 -0700

    getChannels (all untested)

--- main.go
+++ main.go
@@ -70,7 +70,15 @@ func main() {
 	// database.TheTrucksAreHere()
 	database.InitializeDB()
 	registerEndpoints()
-	routes.UserInServer("1", "user")
+	// routes.UserInServer("1", "user")
+	// database.Query([]string{"name", "id"}, "channel", "serverID", "1")
+	// channel := map[string]any{
+	// 	"name":     "fried",
+	// 	"serverID": "1",
+	// }
+
+	// database.AddRowWithIDReturn(channel, "channel")
+
 	fmt.Println("Relay Server active on port 8080")
 	handler := enableCORS(http.DefaultServeMux)
 	err := http.ListenAndServe(":8080", handler)
--- routes/channel.go
+++ routes/channel.go
@@ -12,6 +12,10 @@ type CreateChannelRequest struct {
 	ServerID string `json:"serverID"`
 }
 
+type GetChannelsRequest struct {
+	ServerID string `json:"serverID"`
+}
+
 func CreateChannel(w http.ResponseWriter, r *http.Request) {
 	var data CreateChannelRequest
 	err := json.NewDecoder(r.Body).Decode(&data)
@@ -44,4 +48,27 @@ func CreateChannel(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "created server")
 }
 
-func GetChannels()
+func GetChannels(w http.ResponseWriter, r *http.Request) {
+	var data GetChannelsRequest
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		http.Error(w, "Invalid Credentials", http.StatusUnauthorized)
+		return
+	}
+	user, err := db.AuthHeaderValidation(r)
+	if err != nil {
+		//do something
+	}
+
+	var rawUserID interface{} = user["userID"]
+	userID := fmt.Sprintf("%v", rawUserID) //cooked
+
+	if !UserInServer(data.ServerID, userID) {
+		//FAH
+	}
+
+	// channels, err := db.Query([]string{"name", "id"}, "channel", "serverID", data.ServerID)
+	// fmt.Fprintf(w,channels)
+	//this part do something with it
+
+}
